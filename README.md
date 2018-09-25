76188950f489822cb7c758da7fdf18adcd2fe30f
Mita <mitafrumoasa@gmail.com>
2018-09-25 21:03:01 +0300

    added

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # git_flow_project
+Vika: [home -> more-features]
+Masha: [powerful-skils -> team-members]
+Egor: [quotes -> footer]
---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+let gulp                = require('gulp'),
+ nunjucksRender      = require('gulp-nunjucks-render'),
+ sass                   = require('gulp-sass'),
+ watch               = require('gulp-watch'),
+ cleanCSS               = require('gulp-clean-css'),
+ autoprefixer           = require('gulp-autoprefixer');
