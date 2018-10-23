c2efc16e013243c7bc27e54eed329b9f7c80fe1f
Mita <mitafrumoasa@gmail.com>
2018-10-23 12:07:32 +0300

    updated gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,34 @@
 let gulp                = require('gulp'),
- nunjucksRender      = require('gulp-nunjucks-render'),
+ nunjucksRender 		= require('gulp-nunjucks-render'),
  sass                   = require('gulp-sass'),
- watch               = require('gulp-watch'),
+ watch               	= require('gulp-watch'),
  cleanCSS               = require('gulp-clean-css'),
  autoprefixer           = require('gulp-autoprefixer');
+
+gulp.task('default', function () {
+  gulp.watch(['src/style.scss','src/**/*.scss'], ['sass']);
+  gulp.watch('src/**/*.html', ['html']);
+  gulp.watch('dest/*.css', ['css']);
+});
+
+gulp.task("html", function(){
+	return gulp.src("src/**/*.html")
+		    .pipe(nunjucksRender())
+		   .pipe(gulp.dest('dest'));
+});
+
+gulp.task("sass", function(){
+	return gulp.src(['src/style.scss','src/**/*.scss'])
+		   .pipe(sass())
+		   .pipe(gulp.dest('dest'));
+});
+
+gulp.task("css", function(){
+	return gulp.src("dest/*.css")
+		   .pipe(cleanCSS())
+		   .pipe(autoprefixer({
+	            browsers: ['last 2 versions'],
+	            cascade: false
+	        }))
+		   .pipe(gulp.dest('dest'));
+});
